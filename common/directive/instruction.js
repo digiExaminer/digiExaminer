6af3225c24702417b67cd76c961b282918831235
manish kumar pandey <manish.pandey@aspiringminds.in>
2017-10-02 18:39:05 +0530

    change before 2nd design

--- common/directive/instruction.js
+++ common/directive/instruction.js
@@ -146,26 +146,98 @@ angular.module('app')
 				}
 				
 			}
-		}
-		$scope.prevQuestion = function(id) {
-			currentprev=id-1;
-			if (currentprev <= $scope.allTestseriesQuestion.length && currentprev>=0) {
-				current = $scope.allTestseriesQuestion[currentprev];
+		};
+		$scope.prevquestion = function(id) {
+			currentprev=id-2;
+			if (currentprev < $scope.allTestseriesQuestion.length && currentprev>=0) {
+				$scope.current = $scope.allTestseriesQuestion[currentprev];
 				qdirectionid=$scope.current.qdirectionId;
 				if($scope.qdirection[qdirectionid]!=undefined && $scope.qdirection[qdirectionid]!='')
 				{
-					currentDirection=$scope.qdirection[qdirectionid];
+					$scope.currentDirection=$scope.qdirection[qdirectionid];
 					
 				}else{
-					currentDirection=[];
+					$scope.currentDirection=[];
 					
 				}
 				
 			}
-			
 		}
+		$rootScope.totalquestion={ 
+				
+				"1" : {
+				
+					totaldirection : [ {
+						id : 1
+				
+					}, {
+						id : 2
+					
+					},
+						{
+							id : 3
+						}, {
+							id : 4
+						}, {
+							id : 5
+						}
 
-	} ])
+					]
+				
+			},
+						"2" : {
+				
+					totaldirection : [ {
+						id : 21
+				
+					}, {
+						id : 22
+					
+					},
+						{
+							id : 23
+						}, {
+							id : 24
+						}, {
+							id : 25
+						}
+
+					]
+				
+			},
+			"3" : {
+				
+					totaldirection : [ {
+						id : 31
+				
+					}, {
+						id : 32
+					
+					},
+						{
+							id : 33
+						}, {
+							id : 34
+						}, {
+							id : 35
+						}
+
+					]
+				
+			}
+					
+				
+			}
+		console.log($rootScope.totalquestion);
+			
+
+
+	} ]).filter('slice', function() {
+		cars=$rootScope.totalquestion;
+		  return function(cars, start, end) {
+			    return cars.slice(start, end);
+			  };
+			})
 	.directive('instruction', function() {
 		return {
 			restrict : 'E',
@@ -175,11 +247,44 @@ angular.module('app')
 	}).directive('instructions', function() {
 
 	return {
-		scope : 'false',
+		scope : 'true',
 		restrict : 'E',
 		replace : false,
 		transclude : true,
 		controller : 'instructionctrl',
 		templateUrl : 'common/template/starttest.html'
 	};
-});
+}).directive('countdown', [
+    'Util', '$interval', function(Util, $interval) {
+        return {
+          restrict: 'A',
+          scope: {
+            date: '@'
+          },
+          link: function(scope, element) {
+            var future;
+            date='October 2, 2017 4:00:00';
+            future = new Date(date);
+            $interval(function() {
+              var diff;
+              diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+              return element.text(Util.dhms(diff));
+            }, 1000);
+          }
+        };
+      }
+    ]).factory('Util', [
+      function() {
+        return {
+          dhms: function(t) {
+            var hours, minutes, seconds;
+            hours = Math.floor(t / 3600) % 24;
+            t -= hours * 3600;
+            minutes = Math.floor(t / 60) % 60;
+            t -= minutes * 60;
+            seconds = t % 60;
+            return [hours + 'h', minutes + 'm', seconds + 's'].join(' ');
+          }
+        };
+      }
+    ]);;
